611e6a8364bb29a06d15ce91aea544760b01484a
Louisebrorsen <loubro01@easv365.dk>
2024-04-18 01:48:50 +0200

    yay
    footer færdig.
    - mangler at få fikset de sidste par ting

--- portfolio/src/components/FooterView.vue
+++ portfolio/src/components/FooterView.vue
@@ -3,17 +3,90 @@
         <div class="container">
             <div class="flex flex-col items-center gap-4 py-6">
                 <div class="flex flex-row gap-4">
-                   
+                    <a href="https://dribbble.com/louisebrorsen" class="button-container">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                <g fill="none">
+                                    <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                    <path fill="white" d="M13.774 14.439a32.815 32.815 0 0 1 1.536 7A9.987 9.987 0 0 1 12 22a9.954 9.954 0 0 1-5.567-1.692a12.029 12.029 0 0 1 7.34-5.87Zm7.799.463a10.024 10.024 0 0 1-4.355 5.63a34.79 34.79 0 0 0-1.46-6.468a12.058 12.058 0 0 1 5.815.838m-9.428-4.417c.332.678.643 1.37.93 2.073a14.034 14.034 0 0 0-8.207 6.451a10.079 10.079 0 0 1-2.827-7.922c3.343.708 6.87.502 10.104-.602m7.575-4.841a10.069 10.069 0 0 1 2.24 7.26a14.094 14.094 0 0 0-6.9-.77a34.782 34.782 0 0 0-1.055-2.398a19.05 19.05 0 0 0 5.715-4.092M7.266 3.19a33.098 33.098 0 0 1 3.93 5.502a17.129 17.129 0 0 1-8.776.43A10.027 10.027 0 0 1 7.266 3.19M12 2c2.388 0 4.581.837 6.3 2.234a17.043 17.043 0 0 1-5.22 3.726a35.077 35.077 0 0 0-3.86-5.568A10.005 10.005 0 0 1 12 2" />
+                                </g>
+                            </svg>
+                        </button>
+                        <div class="button-tooltip">
+                            Dribbble <!-- Navnet, der skal vises i boksen -->
+                        </div>
+                    </a>
+                    <a href="https://github.com/Louisebrorsen" class="button-container">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                <path fill="white" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" />
+                            </svg>
+                        </button>
+                        <div class="button-tooltip">
+                            GitHub <!-- Navnet, der skal vises i boksen -->
+                        </div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/louisebrorsen/" class="button-container">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                <path fill="white" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z" />
+                            </svg>
+                        </button>
+                        <div class="button-tooltip">
+                            LinkedIn <!-- Navnet, der skal vises i boksen -->
+                        </div>
+                    </a>
+                    <router-link to="/kontakt" class="button-container">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                <path fill="white" d="M6 17c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6m9-9a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3M3 5v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2" />
+                            </svg>
+                        </button>
+                        <div class="button-tooltip">
+                            Kontakt <!-- Navnet, der skal vises i boksen -->
+                        </div>
+                    </router-link>
                 </div>
             </div>
         </div>
     </div>
+
 </template>
 
-<script setup>
+
+<script setup >
 
 </script>
 
-<style lang="scss" scoped>
+
+<style scoped>
+
+button:hover {
+    transform: scale(1.33); 
+}
+
+.button-container {
+    position: relative;
+}
+
+.button-tooltip {
+    position: absolute;
+    bottom: 100%; /* Placer boksen over knappen */
+    left: 50%; /* Centrer boksen i forhold til knappen */
+    transform: translateX(-50%); /* Juster den vandrette placering for at centrere boksen */
+    background-color: #ccd5c6; /* Baggrundsfarve for boksen */
+    color: rgb(46, 46, 46); /* Tekstfarve for boksen */
+    padding: 0.25rem 0.5rem; /* Tilpas polstring for at justere størrelsen på boksen */
+    border-radius: 4px; /* Afrundede kanter for boksen */
+    opacity: 0; /* Start med at gøre boksen usynlig */
+    transition: opacity 0.3s ease; /* Tilføj en glidende overgangseffekt for opaciteten */
+}
+
+
+.button-container:hover .button-tooltip {
+    opacity: 1; /* Gør boksen synlig ved hover */
+}
+
+
 
 </style>
--- portfolio/src/views/AboutView.vue
+++ portfolio/src/views/AboutView.vue
@@ -1,87 +1,134 @@
 <template>
-  <hero>
-        <h1 class="text-6xl font-bold  text-center text-green">Kort introduktion </h1>
-      <div class="container columns-2 py-10 ">
-        <div class="div ">
-          <div class=" flex py-6 items-center justify-center ">
-          <img class="h-60 sm:h-80 rounded-full" src="../assets/img/mig.jpg" alt="">
-          </div>
+  <div>
 
+    <hero class="bg-gray-100 py-20">
+      <div class="container py-10">
+        <h1 class="text-6xl font-bold text-center text-green mb-4">Kort introduktion</h1>
+        <div class="flex justify-center">
+          <img class="h-60 sm:h-80 rounded-full" src="../assets/img/mig.jpg" alt="Louise Brorsen">
         </div>
-        <h2 class="text-2xl font-bold text-left py-5">
-          Hej! 
-        </h2>
-        <h3 class="text-xl font-semibold text-left py-5">
-          Jeg hedder Louise Norman Brorsen   
-        </h3>
-        <p class="text-left">
-          Jeg er en dedikeret kreativ designer, med en passion for kvalitet.
-          Jeg besidder en stærk faglig baggrund inden for grafisk design med erfaring inden for både print og digitalt medie. 
-          Gennem mine tidligere projekter har jeg demonstreret mine evner inden for brandidentitet, layoutdesign, og brugercentreret design. 
-          Jeg er fortrolig med en bred vifte af designværktøjer og software, og jeg holder mig konstant opdateret 
-          med de nyeste trends og teknologier inden for grafisk design.
-        </p>
+        <h2 class="text-2xl font-bold text-center py-2">Hej!</h2>
+        <h3 class="text-xl font-semibold text-center py-1">Jeg hedder Louise Norman Brorsen</h3>
+        <p class="text-left">Jeg er en dedikeret kreativ designer, med en passion for kvalitet. Jeg besidder en stærk faglig baggrund inden for grafisk design med erfaring inden for både print og digitalt medie. Gennem mine tidligere projekter har jeg demonstreret mine evner inden for brandidentitet, layoutdesign, og brugercentreret design. Jeg er fortrolig med en bred vifte af designværktøjer og software, og jeg holder mig konstant opdateret med de nyeste trends og teknologier inden for grafisk design.</p>
       </div>
-  </hero>
+    </hero>
 
-  <section class="bg-green">
-    <div class="container py-6">
-      <h2 class="text-4xl font-bold text-center py-8">Mit cv</h2>
-      <div class="container columns-2 py-10 ">
-        <div class="div ">
-          <div class=" flex py-6 items-center justify-center ">
-           <p>her kommer pdf til at være</p>
-           <button class="btn"> Mit CV <Icon icon="material-symbols-light:download-rounded" /></button>
+    <section class="bg-green">
+      <div class="container py-6">
+        <h2 class="text-4xl font-bold text-center py-8">Mit CV</h2>
+        <div class="container grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div class="flex py-6 items-center justify-center">
+            <p>Her kommer pdf til at være</p>
+            <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Mit CV
+              <Icon icon="material-symbols-light:download-rounded" />
+            </button>
           </div>
-
-        </div>
-        <h2 class="text-2xl font-bold text-left py-5">
-          mit video cv   
-        </h2>
-        <p class="text-left">
-          Her kommer video cv til at være
-        </p>
-      </div>
-      
-    </div>
-  </section>
-
-  <section class="bg-white">
-    <div class="container py-6">
-      <h2 class="text-4xl font-bold text-center py-8">Mine kompetencer</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div class="card flex flex-col items-center">
-          <img class="size-52 rounded" src="../assets/img/coding.png" alt="">
-          <div class="card-body">
-            <h3 class="text-2xl font-bold">Design og udvikling</h3>
-            <p class="text-center">Design og udvikling af websites, der er brugervenlige og responsive.</p>
-          </div>
-        </div>
-        <div class="card flex flex-col items-center">
-          <img class="size-52 rounded" src="../assets/img/curve.png" alt="">
-          <div class="card-body">
-            <h3 class="text-2xl font-bold">Grafiske elementer</h3>
-            <p class="text-center">Design og produktion af grafiske elementer til visuel præsentation</p>
-          </div>
-        </div>
-        <div class="card flex flex-col items-center">
-          <img class="size-52 rounded" src="../assets/img/content-creator.png" alt="">
-          <div class="card-body">
-            <h3 class="text-2xl font-bold">Foto og video</h3>
-            <p class="text-center">Redigering af billeder og videoer, og indsætte effekter. </p>  
+          <div>
+            <h2 class="text-2xl font-bold text-left py-5">Mit video cv</h2>
+            <p class="text-left">Her kommer video cv til at være</p>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-
+    <section class="bg-white">
+      <div class="container py-6">
+        <h2 class="text-4xl font-bold text-center py-8">Mine kompetencer</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div class="card-s bg-gray-200 shadow-md rounded-lg overflow-hidden">
+            <div class="card-s__content p-6">
+              <p class="card-s__title font-bold text-xl mb-2">Mine skills</p>
+              <p class="card-s__description">Bevæg musen over og se hvilke apps jeg har erfaring med at benytte.</p>
+            </div>
+            <img src="../assets/img/skils.png" alt="">
+          </div>
+          <div class="card-s bg-gray-200 shadow-md rounded-lg overflow-hidden">
+            <div class="card-s__content p-6">
+              <p class="card-s__title font-bold text-xl mb-2">5 ord omkring mig</p>
+              <p class="card-s__description">Bevæg musen over for at se hvilke 5 keywords der vil beskrive mig</p>
+            </div>
+            <img src="../assets/img/keywords.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
-
+import { Icon } from '@iconify/vue';
 </script>
 
 <style scoped>
+.grid {
+  display: grid;
+  gap: 10px;
+  justify-items: center;
+}
+
+.card-s {
+  position: relative;
+  width: 100%;
+  max-width: 300px;
+  height: 200px;
+  background-color: #ccd5c6;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  perspective: 1000px;
+  box-shadow: 0 0 0 5px #ffffff80;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card-s:hover {
+  transform: scale(1.05);
+  box-shadow: 0 8px 16px rgba(255, 255, 255, 0.2);
+}
+
+.card-s__content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: #ccd5c6;
+  transform: rotateX(0deg);
+  transform-origin: bottom;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card-s__title {
+  margin: 0;
+  font-size: 24px;
+  color: #333;
+  font-weight: 700;
+}
+
+.card-s__description {
+  margin: 10px 0 0;
+  font-size: 14px;
+  color: #777;
+  line-height: 1.4;
+}
+
+.card-s img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transform: rotateX(-90deg);
+}
+
+.card-s:hover img {
+  transform: rotateX(0deg);
+}
 
 </style>
+
